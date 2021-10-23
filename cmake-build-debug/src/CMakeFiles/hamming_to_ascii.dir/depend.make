77b00288742c24681607f1f0ace5657531ae1862
Alex G <giasson.alexj@gmail.com>
2021-10-23 10:32:09 -0700

    remove cmake-build-debug from tracking

--- cmake-build-debug/src/CMakeFiles/hamming_to_ascii.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for hamming_to_ascii.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/alex/work/assignments/assignment_2/tests
-# Build directory: /Users/alex/work/assignments/assignment_2/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[template_test]=] "/Users/alex/work/assignments/assignment_2/cmake-build-debug/tests/template_test")
-set_tests_properties([=[template_test]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/alex/work/assignments/assignment_2/tests/CMakeLists.txt;36;add_test;/Users/alex/work/assignments/assignment_2/tests/CMakeLists.txt;0;")
